9626f05570be64e4cc3b2df9dcc33fe1d28c22f7
PabloJoelMR <pabloreufer@gmail.com>
2024-01-31 11:07:20 +0100

    test fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def main(event):
-    print("Hello, world!")
+    return "Hello World!"
+
 
 if __name__ == "__main__":
     main(None)
-
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import unittest
 from main import main
 
-class TestMain(unittest.TestCase):
+class TestMainFunction(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(main(None), "Hello, world!")
+        self.assertEqual(main(None), "Hello World!")
 
 if __name__ == '__main__':
     unittest.main()
